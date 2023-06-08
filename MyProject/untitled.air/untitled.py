1a6f53f0942ca20c3cc4a4f066448b3740db4903
Administrator <986894892@qq.com>
2023-06-08 16:54:17 +0800

    commit

---
+++ MyProject/untitled.air/untitled.py
@@ -0,0 +1,14 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+
+from airtest.core.api import *
+
+auto_setup(__file__)touch(set_current("control_panel")
+set_current("127.0.0.1:62025")
+touch(Template(r"tpl1686213776053.png", record_pos=(0.355, -0.565), resolution=(1440, 2560)))
+touch(Template(r"tpl1686213795392.png", record_pos=(0.385, -0.389), resolution=(1440, 2560)))
+text("13611081148")
+touch(Template(r"tpl1686213829602.png", record_pos=(-0.351, -0.263), resolution=(1440, 2560)))
+text("1234")
+touch(Template(r"tpl1686213847967.png", record_pos=(-0.038, -0.026), resolution=(1440, 2560)))
+)
